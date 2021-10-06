c2def227b7161ab8527aca4f962df9968efe9c4e
szeke <szeke@MATEBOOK13>
2021-10-06 15:58:11 +0200

    Add project files.

---
+++ XS3GNR_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XS3GNR_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ XS3GNR_HFT_2021221.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XS3GNR_HFT_2021221.Repository
+{
+    public class Class1
+    {
+    }
+}
