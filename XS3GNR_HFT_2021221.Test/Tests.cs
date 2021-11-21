adebd4be96a168257cf14f5ef09b67002a66d993
SzekelyAron <AronX@stud.uni-obuda.hu>
2021-11-21 15:34:01 +0100

    Tests.cs created and test NuGet packages installed

---
+++ XS3GNR_HFT_2021221.Test/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XS3GNR_HFT_2021221.Logic;
+using XS3GNR_HFT_2021221.Models;
+using XS3GNR_HFT_2021221.Repository;
+
+namespace XS3GNR_HFT_2021221.Test
+{
+    class Tests
+    {
+
+    }
+}
