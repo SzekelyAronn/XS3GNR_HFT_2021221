855d62fd1e3fb29b7f8593974b232577cf2a06ce
SzekelyAron <AronX@stud.uni-obuda.hu>
2022-03-16 21:33:54 +0100

    ViewModel created, Wpf lists Students

---
+++ XS3GNR_HFT_2021221.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XS3GNR_HFT_2021221.Models;
+
+namespace XS3GNR_HFT_2021221.WPFClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Student> Students { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Students = new RestCollection<Student>("http://localhost:29075/", "student");
+        }
+    }
+}
